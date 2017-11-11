87d1626c9e2446fa29121c7b3d3d580ea517a6b2
John Pellow <pellowj@aol.com>
2017-11-10 21:11:26 -0600

    attempting to fix details page, for more info on selected pizza's

--- details.php
+++ details.php
@@ -1,27 +1,27 @@
 <?php 
 include("inc/functions.php");
-$catalog = full_catalog_array();
-
 if (isset($_GET["id"])) {
-    $id = $_GET["id"];
-    if (isset($catalog[$id])) {
-        $item = $catalog[$id];
-    }
+    $id = filter_input(INPUT_GET,"id",FILTER_SANITIZE_NUMBER_INT);
+    $item = single_item_array($id);
 }
-
-if (!isset($item)) {
+if (empty($item)) {
     header("location:catalog.php");
     exit;
 }
-
 $pageTitle = $item["title"];
 $section = null;
-
 include("inc/header.php"); ?>
 
 <div class="section page">
 
     <div class="wrapper">
+        
+        <div class="breadcrumbs">
+            <a href="catalog.php">Full Catalog</a>
+            &gt; <a href="catalog.php?cat=<?php echo strtolower($item["category"]); ?>">
+            <?php echo $item["category"]; ?></a>
+            &gt; <?php echo $item["title"]; ?>
+        </div>
         
         <div class="media-picture">
     
@@ -37,23 +37,51 @@ include("inc/header.php"); ?>
             <table>
             
                 <tr>
-                    <th>Pizza size <?php echo $item["category"]; ?></th>
+                    <th>Category</th>
+                    <td><?php echo $item["category"]; ?></td>
                 </tr>
                 <tr>
-                    <th>Sauce <?php echo $item["genre"]; ?></th>
+                    <th>Genre</th>
+                    <td><?php echo $item["genre"]; ?></td>
                 </tr>
                 <tr>
-                    <th>Toppings</th>
+                    <th>Format</th>
                     <td><?php echo $item["format"]; ?></td>
                 </tr>
                 <tr>
-                    <th>Time</th>
+                    <th>Year</th>
                     <td><?php echo $item["year"]; ?></td>
                 </tr>
-                <?php if (strtolower($item["category"]) == '10"' || '12"' || '14"' || '16"') { ?>
+                <?php if (strtolower($item["category"]) == "books") { ?>
                 <tr>
-                    <th>Cost</th>
-                    <td><?php echo $item["artist"]; ?></td>
+                    <th>Authors</th>
+                    <td><?php echo implode(", ",$item["author"]); ?></td>
+                </tr>
+                <tr>
+                    <th>Publisher</th>
+                    <td><?php echo $item["publisher"]; ?></td>
+                </tr>
+                <tr>
+                    <th>ISBN</th>
+                    <td><?php echo $item["isbn"]; ?></td>
+                </tr>    
+                <?php } else if (strtolower($item["category"]) == "movies") { ?>
+                <tr>
+                    <th>Director</th>
+                    <td><?php echo implode(", ",$item["director"]); ?></td>
+                </tr>
+                <tr>
+                    <th>Writers</th>
+                    <td><?php echo implode(", ",$item["writer"]); ?></td>
+                </tr>
+                <tr>
+                    <th>Stars</th>
+                    <td><?php echo implode(", ",$item["star"]); ?></td>
+                </tr>
+                <?php } else if (strtolower($item["category"]) == "music") { ?>
+                <tr>
+                    <th>Artist</th>
+                    <td><?php echo implode(", ",$item["artist"]); ?></td>
                 </tr>
                 <?php } ?>
             </table>
@@ -63,6 +91,4 @@ include("inc/header.php"); ?>
     </div>
 
 </div>
-
-<?php
 include("inc/footer.php"); 
